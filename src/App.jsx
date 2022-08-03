402691f5aeda116431f9b5148d977ed45a02da2a
Misael Lopes <mecl.ely@gmail.com>
2022-08-03 14:28:26 +0100

    Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,68 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css'
 
 import './global.css'
-import { Post } from './components/Post'
-import { Sidebar } from './components/Sidebar'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/misaellopes01.png',
+      name: 'Misael Lopes',
+      role: 'Back-end Developer @PCS NET'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋,'
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare'
+      },
+      {
+        type: 'link',
+        content: '#novoprojeto'
+      },
+    ],
+    publishedAt: new Date('2022-08-03 10:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋,'
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare'
+      },
+      {
+        type: 'link',
+        content: '#novoprojeto'
+      },
+    ],
+    publishedAt: new Date('2022-08-01 10:00:00')
+  }
+]
 
 export function App() {
 
@@ -15,14 +72,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Misael Lopes"
-            content="Hi there! I'm Misael Lopes. Developer. Computer Technician. 4th year student of Management Informatics Engineering"
-          />
-          <Post
-            author="Augusto André"
-            content="Hi there! I'm Augusto André, 21 years old. Developer. Computer Technician. 4th year student of Informatics Engineering"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
